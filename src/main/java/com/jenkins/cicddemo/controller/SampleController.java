084cf1b8b1f06a85c1b735783cc42a52e304a96b
Viraj <vkulathunga@virtusa.com>
2022-01-02 16:38:54 +0530

    Initial Commit

---
+++ src/main/java/com/jenkins/cicddemo/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.jenkins.cicddemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String get(){
+        return "Sample Controller app is working";
+    }
+}
